3026bf5c1355202145191dd6c6da1f05f462506f
Michal Škoula <michal@skoula.com>
2026-05-30 18:22:33 +0200

    Revert "feat: add section-block Blade component"
    This reverts commit 03fa18037ebc7b79a72fcb40dff73ad3b2305cfa.

--- resources/views/components/section-block.blade.php
+++
@@ -1,5 +0,0 @@
-<section {{ $attributes->merge(['class' => 'section bg-primary-darkest **:text-gray']) }}>
-    <div class="max-content-width post-container-styles">
-        {{ $slot }}
-    </div>
-</section>

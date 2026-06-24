24e64faf7170aab774125f27ee25126c0afe8277
Michal Škoula <michal@skoula.com>
2026-06-24 17:25:35 +0200

    add todo message to unfinished news listing view

---
+++ resources/views/news/index.blade.php
@@ -0,0 +1,9 @@
+@extends('layouts.website')
+
+@section('content')
+{{--    todo: unfinished --}}
+    <div class="min-h-8/10 flex items-center w-full">
+        <p style="text-align: center">todo: Unfinished, this would render all `News` model record</p>
+    </div>
+
+@endsection
